d6110726c2af475743758f6280dd3b060d1807b5
Tiago Miranda <miranda.f.tiago@gmail.com>
2021-03-25 11:54:39 +0000

    get and populate items

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 
 //Item Controller
-const ItemCtrl = (function(){
+const itemCtrl = (function(){
   //Item Constructor
   const Item = function(id, name, calories){
     this.id= id;
@@ -23,34 +23,60 @@ const ItemCtrl = (function(){
 
   //Public methods
   return {
+    getItems: function(){ 
+      return data.items;
+    },
     logData: function(){
       return data;
     }
   }
-  
+
 })();
 
 //UI Controller
 const UICtrl = (function(){
+  const UISelectors = {
+    itemList: '#item-list'
+  }
 
   //Public methods
   return {
+    populateItemList: function(items){
+      let html = '';
 
+      items.forEach(function(item){
+        html += `
+          <li class="collection-item" id="item-${item.id}">
+            <strong>${item.name}</strong> <em>${item.calories}Calories</em>
+            <a href="# class="secondary-content">
+              <i class="edit-item fa fa-pencil"></i>
+            </a>
+          </li>`;
+      });
+      
+      //Insert list items
+      document.querySelector(UISelectors.itemList).innerHTML = html;
+    }
   }
 
 })();
 
 //App Controller
-const App = (function(ItemCtrl, UICtrl){
+const App = (function(itemCtrl, UICtrl){
 
   //Public methodss
   return {
     init: function() {
-      console.log('Initializing app...')
+  
+      //Fetch items from data structure
+      const items = itemCtrl.getItems();
+      
+      //Populate list with items
+      UICtrl.populateItemList(items);
     }
   }
 
-})(ItemCtrl, UICtrl);
+})(itemCtrl, UICtrl);
 
 //Initialize App
 App.init();
